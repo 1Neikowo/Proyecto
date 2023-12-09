af1c10c86e456d351d5637a8aad138f06f62dac2
GersonUA <g.urrea02@ufromail.cl>
2023-12-09 11:38:08 -0300

    xMVIO

--- src/main/java/Guis/VentanaMostrar.java
+++ src/main/java/Guis/VentanaMostrar.java
@@ -17,15 +17,15 @@ public class VentanaMostrar extends VentanaBase {
     private JPanel panel1;
 
     public VentanaMostrar(AIV aiv) {
-        super("Mostrar Plantas", 500, 520);
+        super("Plantas Almacenadas", 500, 520);
         this.aiv = aiv;
         generarElementosVentana();
     }
 
     public void generarElementosVentana() {
         generarEncabezado();
-        generarTabla();
         generarBotonVolver();
+        generarTabla();
     }
 
     public void generarEncabezado() {
@@ -34,42 +34,23 @@ public class VentanaMostrar extends VentanaBase {
     }
 
     public void generarTabla() {
-       /* DefaultTableModel model = new DefaultTableModel();
+        DefaultTableModel model = new DefaultTableModel();
         model.setColumnIdentifiers(new Object[]{"ID", "Nombre", "Clasificacion", "Tamaño", "Ambiente", "Precio", "Cantidad"});
         ArrayList<Planta> plantas = aiv.obtenerListaDePlantas();
         for (Planta planta : plantas) {
             model.addRow(new Object[]{planta.getId(), planta.getNombre(),planta.getClasificacion(), planta.getTamano(), planta.getAmbiente(), planta.getPrecio(), planta.getCantidad()});
         }
+        // Creamos la tabla después de haber creado el modelo
         table = new JTable(model);
         table.setPreferredScrollableViewportSize(new Dimension(450, 63));
         JScrollPane scrollPane = new JScrollPane(table);
-        getContentPane().add(scrollPane, BorderLayout.CENTER);*/
-        Object[][] datos = {
-                {1, "Planta 1", "Clasificación A", "Mediano", "Interior", 15.99, 10},
-                {2, "Planta 2", "Clasificación B", "Grande", "Exterior", 25.99, 5},
-                {3, "Planta 3", "Clasificación C", "Pequeño", "Interior", 9.99, 20}
-        };
-
-        // Nombres de las columnas
-        String[] columnas = {"ID", "Nombre", "Clasificación", "Tamaño", "Ambiente", "Precio", "Cantidad"};
-
-        // Crear el modelo de la tabla con los datos y columnas
-        DefaultTableModel modelo = new DefaultTableModel(datos, columnas);
-
-        // Crear la JTable con el modelo
-         table = new JTable(modelo);
-        table.setPreferredScrollableViewportSize(new Dimension(600, 200)); // Tamaño preferido de la tabla
-
-        // Crear un JScrollPane y agregar la tabla a él
-        JScrollPane scrollPane = new JScrollPane(table);
-
-        // Crear un JFrame para mostrar la tabla
-        JFrame frame = new JFrame("Ejemplo JTable");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.add(scrollPane, BorderLayout.CENTER);
-
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+        // Actualizamos la interfaz de usuario para mostrar la nueva tabla
+        getContentPane().revalidate();
+        getContentPane().repaint();
     }
+
+
     public void generarBotonVolver() {
         btVolver = generarBotonPrincipal("Volver", 175, 400, 150, 35);
         this.add(btVolver);
